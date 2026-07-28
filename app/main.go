436f18e56cde90de48bbd6aee3682182e951f40e
Minvydas <minderrx2@gmail.com>
2026-07-28 12:33:57 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -19,9 +19,13 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
